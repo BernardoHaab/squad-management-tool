6c7bdcc9fe59d1880033d42a71515a0ad48727b3
BernardoHaab <bernardohaab@gmail.com>
2021-09-07 19:16:45 -0300

    Enable edit button team from TopFive list

--- src/components/TopFiveList.tsx
+++ src/components/TopFiveList.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 import TeamProps from "../types/useTeamProps";
 
@@ -12,11 +13,20 @@ const TopFiveList: React.FC<TopFiveListProps> = ({ title, topFive }) => {
     <div className="list-container">
       <h3>{title}</h3>
       <div className="list">
-        {topFive.map((item, id) => (
-          <div key={id} className="item">
-            <p>{item.name}</p>
-            <p id="team-avg">{item.avgAge}</p>
-          </div>
+        {topFive.map((team, id) => (
+          <Link
+            key={id}
+            href={{
+              pathname: "/CreateTeam",
+              query: { editingTeamId: team.id },
+            }}
+            passHref
+          >
+            <div className="item">
+              <p>{team.name}</p>
+              <p id="team-avg">{team.avgAge}</p>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
