06548c0251e02c04f19a5295642e344fd4dfbb0d
BernardoHaab <bernardohaab@gmail.com>
2021-09-07 16:56:08 -0300

    Correct TopFive list

--- src/components/TopFive.tsx
+++ src/components/TopFive.tsx
@@ -10,19 +10,11 @@ type TopFive = {
 };
 
 const TopFive: React.FC<TopFive> = ({ highestAvgAge, lowestAvgAge }) => {
-  const [sortedHighest, setSortedHighest] = useState<TeamProps[]>([]);
-  const [sortedLowest, setSortedLowest] = useState<TeamProps[]>([]);
-
-  useEffect(() => {
-    setSortedHighest(highestAvgAge.sort((a, b) => b.avgAge - a.avgAge));
-    setSortedLowest(lowestAvgAge.sort((a, b) => a.avgAge - b.avgAge));
-  }, [highestAvgAge, lowestAvgAge]);
-
   return (
     <Card title="Top 5">
       <div className="top-five-content">
-        <TopFiveList title="Highest avg age" topFive={sortedHighest} />
-        <TopFiveList title="Lowest avg age" topFive={sortedLowest} />
+        <TopFiveList title="Highest avg age" topFive={highestAvgAge} />
+        <TopFiveList title="Lowest avg age" topFive={lowestAvgAge} />
       </div>
     </Card>
   );
